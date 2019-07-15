53a78e946186d83f84fd285261a0da86efe94c9d
Tomas Hermanek <hermanekt@users.noreply.github.com>
2019-07-15 18:27:42 +0200

    Update days.js

--- days.js
+++ days.js
@@ -27,7 +27,7 @@ async function days(timezone, ip) {
 
   const date = new Date();
   const year = date.getFullYear();
-  const xmas = parse(`${year }-07-15T00:00:00`);
+  const xmas = parse(`${year }-07-14T00:00:00`);
 
   const now = new Date().getTime() + tzOffset.ms;
   let delta = differenceInDays(xmas, now);
