5448a1f987d9b4a8cc78dff0b17da6f908f428ca
Tomas Hermanek <hermanekt@users.noreply.github.com>
2021-03-29 18:26:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ const handler = async (req, res) => {
     frames: [
       {
         icon,
-        "text":"Patch 1.12 is released"
+        "text":"Patch 1.2 is Finaly released!!!"
         }
     ],
   });
